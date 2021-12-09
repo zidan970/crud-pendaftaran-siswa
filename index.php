c166a589ea01670458b499e5bb2a623807d3fb9c
ZIDAN <55623766+zidan970@users.noreply.github.com>
2021-12-09 08:49:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,56 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
+
 <head>
-    <title>Pendaftaran Siswa Baru | SMK Coding</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    <title>Pendaftaran Siswa Baru</title>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Candal&family=Spinnaker&display=swap');
+    </style>
+    <style>
+        .a 
+        {
+            width: 100%;
+        }
+
+        body 
+        {
+            background-image: linear-gradient(120deg, #AAF683 100%, #FFD97D 0%);
+            font-family: 'Candal', sans-serif;
+            font-family: 'Spinnaker', sans-serif;
+        }
+
+        .mb-3
+        {
+            margin-right: 115px;
+        }
+
+        .mb-4
+        {
+            margin-top: -47px;
+            margin-right: -120px;
+        }
+    </style>
 </head>
 
 <body>
-    <header>
+    <div class="d-flex flex-column justify-content-center align-items-center" style="min-height: 100vh">
         <h3>Pendaftaran Siswa Baru</h3>
         <h1>SMK Coding</h1>
-    </header>
+        
+        <div class="d-flex flex-column align-items-center mt-3">
+            <a class="mb-3" href="form-daftar.php"><button class="btn-danger btn-primary">Daftar Baru</button></a>
+            <a class="mb-4" href="list-siswa.php"><button class="btn-danger btn-primary">Pendaftar</button></a>
+        </div>
+    </div>
+</body>
 
-    <h4>Menu</h4>
-    <nav>
-        <ul>
-            <li><a href="form-daftar.php">Daftar Baru</a></li>
-            <li><a href="list-siswa.php">Pendaftar</a></li>
-        </ul>
-    </nav>
-
-    </body>
 </html>
